2db71110a490f0ac00bf6ec828c6c128eec7501a
Mark  Ridder <mark@kleurbleur.nl>
2019-04-19 18:36:41 +0200

    JS code done. CSS needs work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
-const answer = 5
-const guess = document.forms["numbercheck"]["guess"].value
+localStorage.setItem('attempts', 0)
+const after = document.getElementById('top')
+const box = document.getElementById('box')
 
-if (guess === answer) {
-    
+function checkNumber (){
+
+    const answer = 5
+    const guess = document.forms["numbercheck"]["guess"].value
+
+    localStorage.setItem('attempts',parseInt(localStorage.getItem('attempts'))+1);
+
+    if (localStorage.getItem('attempts') == 3) {
+        // insert html code 
+        let htmlcode = '<div id="toomany">You are out of guesses!</div>' 
+        after.insertAdjacentHTML('afterend', htmlcode)
+        box.parentNode.removeChild(box)
+    }
+
+    if (guess == answer) {
+        // instert html code good
+        let htmlcode = '<div id="result">You guessed right!</div>'
+        after.insertAdjacentHTML('afterend', htmlcode)
+    } else {
+        // instert html code wrong
+        let htmlcode = '<div id="result">You guessed wrong!</div>' 
+        after.insertAdjacentHTML('afterend', htmlcode)
+    }
+}
+
+function clearresult(){
+    console.log(document.getElementById('result'))
+    let result = document.getElementById('result')
+    result.innerHTML = ""
 }
